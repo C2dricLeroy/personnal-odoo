3e4d6de54a1c8b8f14cd138c8602593c76af1259
C2dricLeroy <pro.cedricleroy@gmail.com>
2026-01-26 20:26:13 +0100

    [IMP] blog

--- celer-addons/celer_blog/__manifest__.py
+++ celer-addons/celer_blog/__manifest__.py
@@ -7,5 +7,9 @@
     ],
     "data": [
         "views/website_blog_templates.xml",
+        "views/components/blog_header.xml",
+        "views/components/blog_post_card.xml",
+        "views/components/blog_post_list.xml",
+        "views/components/blog_newsletter.xml",
     ],
 }
